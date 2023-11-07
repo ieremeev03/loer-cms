34a5db360fe3441fc96c03d957cf07c77cb41158
ieremeev03 <54796944+ieremeev03@users.noreply.github.com>
2023-11-07 21:35:13 +0500

    preloader

--- resources/js/Components/Admin/Properties/video.vue
+++ resources/js/Components/Admin/Properties/video.vue
@@ -24,7 +24,7 @@ const emit = defineEmits(['files-dropped'])
 
 const input = ref(null);
 
-
+const loading = ref(false)
 
 defineExpose({ focus: () => input.value.focus() });
 
@@ -32,6 +32,7 @@ function removeFile(field_value) {
     emit('update:modelValue', field_value)
 }
 function onFileChanged($event, item_id, field_id) {
+    this.loading = true;
     let files = $event.target.files || $event.dataTransfer.files;
     console.log(files[0]);
     let formData = new FormData();
@@ -43,10 +44,11 @@ function onFileChanged($event, item_id, field_id) {
     }).then(response => {
         console.log(response.data, item_id, field_id)
         emit('files-dropped', [response.data, item_id, field_id])
-
+        this.loading = false;
     })
         .catch(err => {
             console.log(err)
+            this.loading = false;
         })
 
 
@@ -73,10 +75,19 @@ function onFileChanged($event, item_id, field_id) {
             </div>
         </div>
 
-        <input class="m-0 text-xs" v-if="field_value === null || field_value===''"
-            type="file"
-               @change="onFileChanged($event, item_id, field_id)"
-        >
+        <div class="flex flex-row items-center gap-2">
+            <input  class="m-0 text-xs" v-if="field_value === null || field_value==='' || !loading"
+                    type="file"
+                    @change="onFileChanged($event, item_id, field_id)"
+            >
+
+            <div class="" v-if="loading">
+                <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-indigo-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                    <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                </svg>
+            </div>
+        </div>
 
 
 
