fc812bd4f547fdb5e7f1686a1e4ab4a89da9754b
ieremeev03 <54796944+ieremeev03@users.noreply.github.com>
2023-11-10 15:00:22 +0500

    preloader off

--- resources/js/Layouts/PageLayout.vue
+++ resources/js/Layouts/PageLayout.vue
@@ -2,7 +2,6 @@
 <script setup>
 import Header from "@/Components/Web/Header.vue";
 import Footer from "@/Components/Web/Footer.vue";
-import {VuePreloader} from "vue-preloader";
 import {ref} from "vue";
 const showAnimation = ref(true)
 
@@ -32,23 +31,7 @@ section {
 <template>
     <div class="wrapper">
         <Header :menu="$page.props.menus['main']"/>
-        <VuePreloader
-            class="preloader"
-            transition-type="fade-up"
-            :loading-speed="25"
-            :transition-speed="500"
-            @loading-is-over="showAnimation = false"
-        >
-            <template v-slot="{ percent, color }">
-                <transition name="loading-animation" mode="in-out">
-                      <div style="text-align: center" v-if="showAnimation">
-                          Загрузка<br><br>
-                        {{percent }} %
-                      </div>
-                </transition>
-            </template>
 
-        </VuePreloader>
         <slot/>
         <!--Подвал-->
         <Footer :menu="$page.props.menus['bottom']"/>
