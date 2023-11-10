c03d47fc77c7fe52a82c4e6dfadb1b4aaa7e6fc1
ieremeev03 <54796944+ieremeev03@users.noreply.github.com>
2023-11-10 17:04:10 +0500

    section size

--- resources/js/Components/Web/Events.vue
+++ resources/js/Components/Web/Events.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="section__services" v-bind:class="'section__'+properties?.color">
+    <section class="section__services" style="height: 100vh" v-bind:class="'section__'+properties?.color">
         <div class="container section__services-container">
             <h2 v-if="!properties?.title_prop" class="section__services-title">{{ title }} </h2>
             <h2 v-if="properties?.title_prop" class="section__services-title">{{ properties?.title_prop }} </h2>
@@ -9,8 +9,9 @@
             <a v-if="properties?.link" :href="properties?.link" class="button__more">{{properties?.link_text}}</a>
 
         </div>
+        <slider-h-wd :items="items"/>
     </section>
-    <slider-h-wd :items="items"/>
+
 </template>
 
 <script>
--- resources/js/Components/Web/Poster.vue
+++ resources/js/Components/Web/Poster.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="section__services" v-bind:class="'section__'+properties.color">
+    <section class="section__services" style="height: 100vh" v-bind:class="'section__'+properties.color">
         <div class="container section__services-container">
             <h2 v-if="!properties?.title_prop" class="section__services-title">{{ title }} </h2>
             <h2 v-if="properties?.title_prop" class="section__services-title">{{ properties?.title_prop }} </h2>
@@ -8,8 +8,9 @@
             <div v-if="properties?.content" class="section__services-text" v-html="properties?.content"></div>
             <a v-if="properties?.link" :href="properties?.link" class="button__more">{{properties?.link_text}}</a>
         </div>
+        <slider-h :items="items"/>
     </section>
-    <slider-h :items="items"/>
+
 </template>
 
 <script>
