91244897605797b85ec24132e6010ab0b73cb3a8
ieremeev03 <54796944+ieremeev03@users.noreply.github.com>
2023-11-15 17:16:52 +0500

    add value properties2

--- resources/js/Components/Web/Events.vue
+++ resources/js/Components/Web/Events.vue
@@ -1,11 +1,11 @@
 <template>
     <section class="section__services"  v-bind:class="'section__'+properties?.color.value">
         <div class="container section__services-container">
-            <h2 v-if="!properties?.title_prop" class="section__services-title">{{ title }} </h2>
-            <h2 v-if="properties?.title_prop" class="section__services-title">{{ properties?.title_prop }} </h2>
+            <h2 v-if="!properties?.title_prop?.value" class="section__services-title">{{ title }} </h2>
+            <h2 v-if="properties?.title_prop?.value" class="section__services-title">{{ properties?.title_prop?.value }} </h2>
 
-            <div v-if="!properties?.content" class="section__services-text" v-html="content"></div>
-            <div v-if="properties?.content" class="section__services-text" v-html="properties?.content"></div>
+            <div v-if="!properties?.content?.value" class="section__services-text" v-html="content"></div>
+            <div v-if="properties?.content?.value" class="section__services-text" v-html="properties?.content?.value"></div>
             <a v-if="properties?.link.value" :href="properties?.link.value" class="button__more">{{properties?.link_text.value}}</a>
 
         </div>
