398121954977d9870988be6f217ea8493a2ba626
ieremeev03 <54796944+ieremeev03@users.noreply.github.com>
2023-11-17 23:54:45 +0500

    style fix

--- resources/js/Components/Web/HowBlock.vue
+++ resources/js/Components/Web/HowBlock.vue
@@ -1,11 +1,11 @@
 <template>
     <section class="section__how">
         <div class="container section__how-container">
-            <div class="section__how-block section__white">
+            <div class="section__how-block" v-bind:class="'section__'+properties.left_color.value">
                 <h2 class="section__services-title"><a href="/howtoget">Как добраться</a></h2>
                 <div class="section__services-text">Скорый электропоезд "Ласточка" довезет Вас до Чусового,где производится пересадка на автобусный трансфер до Горнолыжного курорта "Такман".</div>
             </div>
-            <div class="section__how-block section__black">
+            <div class="section__how-block" v-bind:class="'section__'+properties.left_color.value">
                 <h2 class="section__services-title">График работы</h2>
                 <div class="section__how-block-columns">
                     <div class="section__how-block-column">
