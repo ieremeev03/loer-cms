79cec7e49b06d51ff6704ffc4e78a22ece92b6ab
ieremeev03 <54796944+ieremeev03@users.noreply.github.com>
2023-11-17 23:51:01 +0500

    style fix

--- resources/js/Components/Web/HowBlock.vue
+++ resources/js/Components/Web/HowBlock.vue
@@ -1,13 +1,34 @@
 <template>
     <section class="section__how">
         <div class="container section__how-container">
-            <div class="section__how-block" v-bind:class="'section__'+properties.left_color.value">
-                <h2 class="section__services-title">{{properties.left_title.value}}</h2>
-                <div class="section__services-text" v-html="properties.left_content.value"></div>
+            <div class="section__how-block section__white">
+                <h2 class="section__services-title"><a href="">Как добраться</a></h2>
+                <div class="section__services-text">Скорый электропоезд "Ласточка" довезет Вас до Чусового,где производится пересадка на автобусный трансфер до Горнолыжного курорта "Такман".</div>
             </div>
-            <div class="section__how-block" v-bind:class="'section__'+properties.right_color.value">
-                <h2 class="section__services-title">{{properties.right_title.value}}</h2>
-                <div class="section__services-text" v-html="properties.right_content.value"></div>
+            <div class="section__how-block section__black">
+                <h2 class="section__services-title">График работы</h2>
+                <div class="section__how-block-columns">
+                    <div class="section__how-block-column">
+                        <div class="section__how-block-item">
+                            <div class="section__how-block-item-title">Администрация</div>
+                            <div class="section__how-block-item-text"><p>Ежедневно</p> <p>с 9:00 до 23:00</p></div>
+                        </div>
+                        <div class="section__how-block-item">
+                            <div class="section__how-block-item-title">Кафе</div>
+                            <div class="section__how-block-item-text"><p>Ежедневно</p> <p>с 9:00 до 23:00</p></div>
+                        </div>
+                    </div>
+                    <div class="section__how-block-column">
+                        <div class="section__how-block-item">
+                            <div class="section__how-block-item-title">Прокат</div>
+                            <div class="section__how-block-item-text"><p>Ежедневно</p> <p>с 9:00 до 23:00</p></div>
+                        </div>
+                        <div class="section__how-block-item">
+                            <div class="section__how-block-item-title">Инструкторы </div>
+                            <div class="section__how-block-item-text"><p>Ежедневно</p> <p>с 9:00 до 23:00</p></div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
@@ -21,5 +42,9 @@ export default {
 </script>
 
 <style scoped>
+
+.section__services-title a {
+    color: #181F26;
+}
 
 </style>
