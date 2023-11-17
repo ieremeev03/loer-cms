b0dd52e214dc3393c6ec4e63fc7fee34642ec9e0
ieremeev03 <54796944+ieremeev03@users.noreply.github.com>
2023-11-17 17:50:32 +0500

    fileList

--- resources/js/Components/Web/FileListContent.vue
+++ resources/js/Components/Web/FileListContent.vue
@@ -5,7 +5,7 @@
             <h2 v-if="!properties.title_prop.value" class="section__services-title">{{ title }} </h2>
             <h2 v-if="properties.title_prop.value" class="section__services-title">{{ properties.title_prop.value }} </h2>
             <div class="section__rules-rules">
-                <a v-for="item in items" :href="item.fields.file?.value" class="section__rules-rule rule-pdf">{{item.fields.title?.value}}
+                <a v-for="item in items" :href="item.fields.file?.value" target="_blank" class="section__rules-rule rule-pdf">{{item.fields.title?.value}}
                 </a>
             </div>
         </div>
