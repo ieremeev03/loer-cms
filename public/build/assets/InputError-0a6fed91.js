013d510a706ea051f3a98aa54dae76cfc726517e
ieremeev03 <54796944+ieremeev03@users.noreply.github.com>
2023-11-27 16:38:16 +0500

    style fix

---
+++ public/build/assets/InputError-0a6fed91.js
@@ -0,0 +1,1 @@
+import{k as t,bu as s,o as a,c as r,b as o,t as c}from"./app-c01e0ddf.js";const n={class:"text-sm text-red-600 dark:text-red-400"},l={__name:"InputError",props:{message:{type:String}},setup(e){return(i,m)=>t((a(),r("div",null,[o("p",n,c(e.message),1)],512)),[[s,e.message]])}};export{l as _};
