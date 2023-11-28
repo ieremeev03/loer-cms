97312e2b6c5bb43428bb967cda30fb8a27fb8aa2
ieremeev03 <54796944+ieremeev03@users.noreply.github.com>
2023-11-28 16:09:08 +0500

    photo fix

--- resources/js/Components/Web/Photo.vue
+++ resources/js/Components/Web/Photo.vue
@@ -5,6 +5,16 @@
                 <source :srcset="properties.photo.value" type="image/avif">
                 <img :src="properties.photo.value" alt="">
             </picture>
+
+            <picture>
+                <source :srcset="properties.photo1?.value" media="(min-width: 320px) and (max-width: 480px)">
+                <source :srcset="properties.photo2?.value" media="(min-width: 481px) and (max-width: 767px)">
+                <source :srcset="properties.photo3?.value" media="(min-width: 768px) and (max-width: 1024px) and (orientation: landscape)">
+                <source :srcset="properties.photo4?.value" media="(min-width: 768px) and (max-width: 1024px)">
+                <source :srcset="properties.photo5?.value" media="(min-width: 1025px) and (max-width: 1280px)">
+                <source :srcset="properties.photo?.value" media="(min-width: 1281px)">
+                <img loading="lazy" :src="properties.photo?.value" alt="">
+            </picture>
         </a>
     </section>
 </template>
