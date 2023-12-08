e5734cd59228bae1d35647db28e2ce993f08de25
ieremeev03 <54796944+ieremeev03@users.noreply.github.com>
2023-12-08 14:00:22 +0500

    модуль бронирования

--- resources/js/Components/Web/BookingHorizontal.vue
+++ resources/js/Components/Web/BookingHorizontal.vue
@@ -1,8 +1,25 @@
 <template>
-    <div id="WidgetIdBooking"></div>
+    <section class="section__services"  v-bind:class="'section__'+properties.color.value">
+        <div class="container section__services-container">
+            <h2 v-if="!properties?.title_prop?.value" class="section__services-title">{{ title_block }} </h2>
+            <h2 v-if="properties?.title_prop?.value" class="section__services-title">{{ properties?.title_prop?.value }} </h2>
+
+            <div v-if="!properties?.content?.value" class="section__services-text" v-html="content"></div>
+            <div v-if="properties?.content?.value" class="section__services-text" v-html="properties?.content?.value"></div>
+            <div id="WidgetIdBooking"></div>
+        </div>
+    </section>
 </template>
 
+
 <script setup>
+
+const props = defineProps({
+    title: String,
+    properties: Array,
+    content: String,
+});
+
 (function(k,o,t,e,l){
     l = document.createElement("script");
     l.type = "text/javascript";
@@ -16,7 +33,6 @@
     t.parentNode.insertBefore(l, t);
 })(function(){
     HotelWidget.init({
-
         id: "aaba9e02-6081-4a8e-a6b3-baf1e48db86c",
         type: "horizontalBlock",
         form: {
