81c02553ec97b581369c07f77283bdc6d4119f98
ieremeev03 <54796944+ieremeev03@users.noreply.github.com>
2023-12-11 11:26:43 +0500

    вывод параметром из инфоблока если на странице они пустые

--- public/build/assets/ApplicationLogo-2c1e9a84.js
+++
@@ -1,1 +0,0 @@
-import{bH as o,o as c,c as s}from"./app-855fd9c3.js";const t={},e={src:"/assets/img/logo.svg",alt:"",width:"200"};function n(r,a){return c(),s("img",e)}const i=o(t,[["render",n]]);export{i as A};
