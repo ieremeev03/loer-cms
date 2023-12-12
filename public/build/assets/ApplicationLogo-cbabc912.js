6f48b6ed7d1278b36fd23a415001fd4fd0846b83
ieremeev03 <54796944+ieremeev03@users.noreply.github.com>
2023-12-12 11:01:33 +0500

    приборка в админке 1

---
+++ public/build/assets/ApplicationLogo-cbabc912.js
@@ -0,0 +1,1 @@
+import{bH as o,o as c,c as s}from"./app-7c9c526d.js";const t={},e={src:"/assets/img/logo.svg",alt:"",width:"200"};function n(r,a){return c(),s("img",e)}const i=o(t,[["render",n]]);export{i as A};
--- resources/js/Pages/Admin/Instructors/Index.vue
+++ resources/js/Pages/Admin/Instructors/Index.vue
@@ -29,7 +29,7 @@
                             </thead>
                             <tbody class="divide-y divide-gray-200 bg-white">
                             <tr v-for="instructor in instructors">
-                                <td class="whitespace-nowrapp p4 text-sm font-medium text-gray-900  ">{{ instructor.name }}</td>
+                                <td class="whitespace-nowrapp p4 text-sm font-medium text-gray-900  "><a :href="route('instructor.edit', instructor.id)" class="hover:text-blue-500">{{ instructor.name }}</a></td>
                                 <td class="whitespace-nowrap p-4 text-sm font-medium text-gray-900  ">
                                     <span v-for="discipline in instructor.disciplines" class="mr-2">
                                         {{ discipline.name }}
@@ -40,12 +40,12 @@
                                         <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path fill-rule="evenodd" clip-rule="evenodd" d="m3.99 16.854-1.314 3.504a.75.75 0 0 0 .966.965l3.503-1.314a3 3 0 0 0 1.068-.687L18.36 9.175s-.354-1.061-1.414-2.122c-1.06-1.06-2.122-1.414-2.122-1.414L4.677 15.786a3 3 0 0 0-.687 1.068zm12.249-12.63 1.383-1.383c.248-.248.579-.406.925-.348.487.08 1.232.322 1.934 1.025.703.703.945 1.447 1.025 1.934.058.346-.1.677-.348.925L19.774 7.76s-.353-1.06-1.414-2.12c-1.06-1.062-2.121-1.415-2.121-1.415z" fill="currentColor"/></svg>
                                     </a>
-                                    <a href="#" @click="" class="font-medium text-blue-600  hover:underline">
+<!--                                    <a href="#" @click="" class="font-medium text-blue-600  hover:underline">
                                         <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path fill-rule="evenodd" clip-rule="evenodd" d="M21 8C21 6.34315 19.6569 5 18 5H10C8.34315 5 7 6.34315 7 8V20C7 21.6569 8.34315 23 10 23H18C19.6569 23 21 21.6569 21 20V8ZM19 8C19 7.44772 18.5523 7 18 7H10C9.44772 7 9 7.44772 9 8V20C9 20.5523 9.44772 21 10 21H18C18.5523 21 19 20.5523 19 20V8Z" fill="currentColor"/>
                                             <path d="M6 3H16C16.5523 3 17 2.55228 17 2C17 1.44772 16.5523 1 16 1H6C4.34315 1 3 2.34315 3 4V18C3 18.5523 3.44772 19 4 19C4.55228 19 5 18.5523 5 18V4C5 3.44772 5.44772 3 6 3Z" fill="currentColor"/>
                                         </svg>
-                                    </a>
+                                    </a>-->
                                     <a href="#" @click="" class="font-medium text-red-600 dark:text-red-500 hover:underline">
                                         <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M9.17065 4C9.58249 2.83481 10.6937 2 11.9999 2C13.3062 2 14.4174 2.83481 14.8292 4" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"/>
