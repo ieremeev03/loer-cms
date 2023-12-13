1bcdab709b01e4c48782172084604273163ed6d5
ieremeev03 <54796944+ieremeev03@users.noreply.github.com>
2023-12-13 10:51:14 +0500

    фавикон apple

--- resources/js/Pages/Page.vue
+++ resources/js/Pages/Page.vue
@@ -4,8 +4,8 @@
         <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,regular,500,600,700,800,900,100italic,200italic,300italic,italic,500italic,600italic,700italic,800italic,900italic" rel="stylesheet" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css">
         <link rel="stylesheet" href="assets/css/style.css">
-        <link rel="shortcut icon" href="/favicon.png" type="image/png">
-        <link rel="apple-touch-icon"  href="/favicon.png" >
+        <link rel="shortcut icon" href="/favicon-32x32.png" type="image/png">
+        <link rel="apple-touch-icon"  href="/apple-touch-icon.png" >
         <meta name="description" content="Takman">
         <meta name="keywords" content="Takman">
         <meta name="robots" content="index, follow">
