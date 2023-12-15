832536d4d9044941f02d069afec5621fd8e63d0e
ieremeev03 <54796944+ieremeev03@users.noreply.github.com>
2023-12-15 20:32:56 +0500

    отключение кнопки

--- public/build/assets/ApplicationLogo-73f341de.js
+++
@@ -1,1 +0,0 @@
-import{bH as o,o as s,d as t}from"./app-65f094bf.js";const c={},e={src:"/assets/img/logo.svg",alt:"",width:"200"};function n(r,a){return s(),t("img",e)}const i=o(c,[["render",n]]);export{i as A};
